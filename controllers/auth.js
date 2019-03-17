6cd6b6b4b4e4af6d6997bfc5c636420ac8cdf7b7
samipkalyani <samipkalyani004@gmail.com>
2019-03-17 20:50:34 +0530

    order fix and added flash messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,55 +1,73 @@
 const User = require('../models/user');
 const bcrypt=require('bcryptjs');
 exports.getLogin = (req, res, next) => {
+  let message =req.flash('error');
+  if(message.length >0){
+    message=message[0];
+  }
+  else{
+    message=null;
+  }
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false
+    errorMessage:message
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message =req.flash('error');
+  if(message.length >0){
+    message=message[0];
+  }
+  else{
+    message=null;
+  }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    isAuthenticated: false
+    errorMessage:message
   });
 };
 
 
 exports.postLogin = (req, res, next) => {
-  const email =req.body.email;
-  const password =req.body.password;
-  User.findOne({email:email})
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
     .then(user => {
-      if(!user){
+      if (!user) {
+        req.flash('error', 'Invalid email or password.');
         return res.redirect('/login');
       }
-      bcrypt.compare(password,user.password).then(doMatch=>{
-          if(doMatch){
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
             req.session.isLoggedIn = true;
             req.session.user = user;
             return req.session.save(err => {
               console.log(err);
               res.redirect('/');
-            });           
+            });
           }
+          req.flash('error', 'Password does not match!');
           res.redirect('/login');
-      }).catch(err=>{
-        console.log(err);
-        res.redirect('/login')
-      })
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
     })
     .catch(err => console.log(err));
 };
-
 exports.postSignup = (req, res, next) => {
   const email =req.body.email;
   const password=req.body.password;
   const confirmPassword =req.body.confirmPassword;
   User.findOne({email:email}).then(userDoc =>{
     if(userDoc){
-      console.log('USER ALREADY EXISTS!');
+      req.flash('error', 'E-mail already exists!.Please use a different one!');
       res.redirect('/signup');
     }
     return bcrypt.hash(password,12)
--- models/order.js
+++ models/order.js
@@ -12,7 +12,7 @@ const orderSchema = new Schema({
     ],
     user: 
     {
-        name:
+        email:
         {
             type: String,
             required: true
