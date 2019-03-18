0fe10408cc9f3397c43aa5c1edbb8a0236842442
samipkalyani <samipkalyani004@gmail.com>
2019-03-19 02:19:54 +0530

    mails and reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,17 @@
+//GLOBAL MODULES
+const crypto = require('crypto');
+//CUSTOM MODULES AND MODELS
 const User = require('../models/user');
 const bcrypt=require('bcryptjs');
+const nodemailer =require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'odecode004@gmail.com',
+    pass: '**********'
+  }
+});
 exports.getLogin = (req, res, next) => {
   let message =req.flash('error');
   if(message.length >0){
@@ -61,29 +73,55 @@ exports.postLogin = (req, res, next) => {
     })
     .catch(err => console.log(err));
 };
-exports.postSignup = (req, res, next) => {
-  const email =req.body.email;
-  const password=req.body.password;
-  const confirmPassword =req.body.confirmPassword;
-  User.findOne({email:email}).then(userDoc =>{
-    if(userDoc){
-      req.flash('error', 'E-mail already exists!.Please use a different one!');
-      res.redirect('/signup');
-    }
-    return bcrypt.hash(password,12)
-    .then(hashedPassword=>{
-      const user =new User({email:email,password:hashedPassword,cart:{items:[]}});
-      return user.save();
-    })
-    .then(result=>{
-      res.redirect('/login');
-    })
-  })
-  .catch(err =>{
-    console.log(err);
-  });
 
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        req.flash(
+          'error',
+          'E-Mail exists already, please pick a different one.'
+        );
+        return res.redirect('/signup');
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.redirect('/login');
+          var mailOptions = {
+            from: 'odecode004@gmail.com',
+            to: email,
+            subject: 'Welcome to your new myshop account',
+            text: `Hi ${email},\n\nWe're so glad you decided to try out myshop. Here are a few tips to get you up and running fast.\n\nHappy shopping!\n\nEnjoy your new account,\n\nadmin@myshop.in`
+          };
+          return transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
+
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
@@ -91,3 +129,54 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+exports.getReset =(req,res,next)=>{
+  let message =req.flash('error');
+  if(message.length >0){
+    message=message[0];
+  }
+  else{
+    message=null;
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Rest',
+    errorMessage:message
+  });
+};
+
+exports.postReset =(req,res,next)=>{
+  crypto.randomBytes(32,(err,buffer)=>{
+    if(err){
+      console.log(err);
+      return res.redirect('/login');
+    }
+    User.findOne({email:email}).then(user=>{
+      if(!user){
+        req.flash('error','No account found with that email ID.');
+        return res.redirect('/login');
+      }
+      user.resetToken = token;
+      user.resetTokenExpiration = Date.now() + 3600000;
+      return user.save();
+    }).then(result =>{
+      res.redirect('/');
+      var mailOptions = {
+        from: 'odecode004@gmail.com',
+        to: email,
+        subject: 'Welcome to your new myshop account',
+        text: `Hi ${email},\nI’m so glad you decided to try out myshop. Here are a few tips to get you up and running fast.\n\nHappy shopping!\n\nEnjoy your new account,\n\nadmin@myshop.in`
+      };
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  })
+}
