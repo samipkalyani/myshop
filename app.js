f2bbcc61ba7eec8cacea7a70d2b44972a7ca3b62
samipkalyani <samipkalyani004@gmail.com>
2019-04-02 19:38:33 +0530

    file upload fix

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const fileStorage = multer.diskStorage({
     cb(null,'images');
   },
   filename: (req,file,cb)=>{
-    cb(null,new Date().toISOString +"-"+file.originalname);
+    cb(null, new Date().toISOString() + '-' + file.originalname);
   }
 });
 
@@ -56,6 +56,7 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({storage:fileStorage,fileFilter: Filefilter}).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use( 
   session (
     {secret: 'my secret',
